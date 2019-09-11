fc63fc90ec84aac50a0cfedbadeee3369c6327e7
blurbyte <info@blurbyte.com>
2019-09-11 15:15:07 +0200

    Create API schema

---
+++ src/schema.rs
@@ -0,0 +1,24 @@
+use serde::{Deserialize, Serialize};
+use chrono::NaiveDateTime;
+
+use crate::utils::validate_rating;
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Game {
+    pub id: u64,
+    pub title: String,
+    #[serde(with = "validate_rating")]
+    pub rating: u8,
+    pub genre: Genre,
+    pub description: String,
+    pub release_date: NaiveDateTime,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum Genre {
+    RolePlaying,
+    Strategy,
+    Shooter,
+}
