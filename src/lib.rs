d49f18e59bf3ce9576787496fdd0dd6dbde77f87
blurbyte <info@blurbyte.com>
2019-09-11 15:16:38 +0200

    Update project structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+pub mod routes;
+mod schema;
+mod utils;
+
 #[cfg(test)]
 mod tests {
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
+use warp::Filter;
+
+use restful_rust::routes::games;
+
 fn main() {
-    println!("Hello, world!");
+    // Show debug logs by default by setting `RUST_LOG=restful_rust=debug`
+    if env::var_os("RUST_LOG").is_none() {
+        env::set_var("RUST_LOG", "restful_rust=debug");
+    }
+    pretty_env_logger::init();
+
+    let games = games::games_route();
+
+    let routes = games.with(warp::log("restful_rust"));
+
+    warp::serve(routes).run(([127, 0, 0, 1], 8080));
 }
