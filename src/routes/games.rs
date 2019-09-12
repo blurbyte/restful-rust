2e2f7d34ee8ad065a9e652f54c1e74a9860aa73f
blurbyte <info@blurbyte.com>
2019-09-12 15:29:56 +0200

    Format

--- src/routes/games.rs
+++ src/routes/games.rs
@@ -12,23 +12,23 @@ pub fn games_route() -> BoxedFilter<(impl Reply,)> {
     // For presentation purposes keep mocked data in in-memory structure
     let db = Arc::new(Mutex::new(
         vec![
-        Game {
-            id: 1,
-            title: String::from("Dark Souls"),
-            rating: 91,
-            genre: Genre::RolePlaying,
-            description: String::from("Takes place in the fictional kingdom of Lordran, where players assume the role of a cursed undead character who begins a pilgrimage to discover the fate of their kind.."),
-            release_date: NaiveDate::from_ymd(2011, 9, 22).and_hms(0, 0, 0),
-        },
-        Game {
-            id: 2,
-            title: String::from("Dark Souls 2"),
-            rating: 87,
-            genre: Genre::RolePlaying,
-            description: String::from("It keeps in line with its predecessors in the Souls series by providing players with a deep but concealed story that must be pieced together via NPC dialogue, item descriptions, appearance, and geographic clues."),
-            release_date: NaiveDate::from_ymd(2014, 3, 11).and_hms(0, 0, 0),
-        },
-    ]
+            Game {
+                id: 1,
+                title: String::from("Dark Souls"),
+                rating: 91,
+                genre: Genre::RolePlaying,
+                description: String::from("Takes place in the fictional kingdom of Lordran, where players assume the role of a cursed undead character who begins a pilgrimage to discover the fate of their kind.."),
+                release_date: NaiveDate::from_ymd(2011, 9, 22).and_hms(0, 0, 0),
+            },
+            Game {
+                id: 2,
+                title: String::from("Dark Souls 2"),
+                rating: 87,
+                genre: Genre::RolePlaying,
+                description: String::from("It keeps in line with its predecessors in the Souls series by providing players with a deep but concealed story that must be pieced together via NPC dialogue, item descriptions, appearance, and geographic clues."),
+                release_date: NaiveDate::from_ymd(2014, 3, 11).and_hms(0, 0, 0),
+            },
+        ]
     ));
 
     let db = warp::any().map(move || db.clone());
