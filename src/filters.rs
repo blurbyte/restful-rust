10ff835ef9db0e0166b93be362788b5e2ff96cba
blurbyte <info@blurbyte.com>
2019-09-17 11:25:47 +0200

    Add json_body filter

--- src/filters.rs
+++ src/filters.rs
@@ -3,6 +3,8 @@
 use serde::Deserialize;
 use warp::{filters::BoxedFilter, Filter};
 
+use crate::schema::Game;
+
 #[derive(Deserialize, Debug)]
 pub struct ListOptions {
     pub offset: Option<usize>,
@@ -12,4 +14,11 @@ pub struct ListOptions {
 // Optional query params to allow pagination
 pub fn list_options() -> BoxedFilter<(ListOptions,)> {
     warp::query::<ListOptions>().boxed()
+}
+
+// Accept only JSON body and reject big payloads
+pub fn json_body() -> BoxedFilter<(Game,)> {
+    warp::body::content_length_limit(1024 * 32)
+        .and(warp::body::json())
+        .boxed()
 }
