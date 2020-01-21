43da6b274882bd4145c87354906573652157a76d
blurbyte <info@blurbyte.com>
2020-01-21 13:15:48 +0100

    Move example database to schema file

--- src/schema.rs
+++ src/schema.rs
@@ -1,5 +1,8 @@
 use chrono::NaiveDateTime;
 use serde::{Deserialize, Serialize};
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use chrono::prelude::*;
 
 use crate::validators;
 
@@ -23,11 +26,46 @@ pub enum Genre {
     Shooter,
 }
 
+// For presentation purposes keep mocked data in in-memory structure
+// In real life scenario connection with regular database would be established
+
+pub type Db = Arc<Mutex<Vec<Game>>>;
+
+pub fn example_db() -> Db {
+    Arc::new(Mutex::new(
+        vec![
+        Game {
+            id: 1,
+            title: String::from("Dark Souls"),
+            rating: 91,
+            genre: Genre::RolePlaying,
+            description: Some(String::from("Takes place in the fictional kingdom of Lordran, where players assume the role of a cursed undead character who begins a pilgrimage to discover the fate of their kind.")),
+            release_date: NaiveDate::from_ymd(2011, 9, 22).and_hms(0, 0, 0),
+        },
+        Game {
+            id: 2,
+            title: String::from("Dark Souls 2"),
+            rating: 87,
+            genre: Genre::RolePlaying,
+            description: None,
+            release_date: NaiveDate::from_ymd(2014, 3, 11).and_hms(0, 0, 0),
+        },
+        Game {
+            id: 3,
+            title: String::from("Dark Souls 3"),
+            rating: 89,
+            genre: Genre::RolePlaying,
+            description: Some(String::from("The latest chapter in the series with its trademark sword and sorcery combat and rewarding action RPG gameplay.")),
+            release_date: NaiveDate::from_ymd(2016, 3, 24).and_hms(0, 0, 0),
+        },
+    ]
+    ))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    use chrono::prelude::*;
     use serde_json::error::Error;
     use serde_test::{assert_tokens, Token};
 
